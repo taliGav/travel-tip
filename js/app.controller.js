ec2d35950c1e8743321a63a5a7a64c00ff5d40c0
Tali <taligavrielov@gmail.com>
2022-02-23 22:26:41 +0200

    onSearchPlace func added

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,7 +1,7 @@
-'use strict'
+'use strict';
 
-import { locService } from './services/loc.service.js'
-import { mapService } from './services/map.service.js'
+import { locService } from './services/loc.service.js';
+import { mapService } from './services/map.service.js';
 
 var gMap;
 
@@ -12,6 +12,7 @@ window.onPanTo = onPanTo;
 window.onGetLocs = onGetLocs;
 window.onGetUserPos = onGetUserPos;
 window.onRemoveLoc = onRemoveLoc;
+window.onSearchPlace = onSearchPlace;
 
 function onInit() {
     mapService.initMap()
@@ -19,10 +20,10 @@ function onInit() {
             console.log(res);
             console.log('Map is ready');
         })
-        
+
         .then(() => {
             console.log('google available');
-            const myLatlng = { lat: 32.0749831, lng: 34.9120554 }
+            const myLatlng = { lat: 32.0749831, lng: 34.9120554 };
             gMap = new google.maps.Map(
                 document.querySelector('#map'), {
                 center: myLatlng,
@@ -73,9 +74,46 @@ function onInit() {
 function getPosition() {
     console.log('Getting Pos');
     return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
 }
+
+function onSearchPlace() {
+    var place = document.querySelector('.search-input').value; 
+    console.log('place',place);
+    const request = {
+        query: place,
+        fields: ["name", "geometry"],
+    };
+
+    var service = new google.maps.places.PlacesService(gMap);
+    service.findPlaceFromQuery(request, (results, status) => {
+        console.log('status',status);
+        if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+            for (let i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+
+            gMap.setCenter(results[0].geometry.location);
+        }
+    });
+}
+
+function createMarker(place) {
+    console.log('place.name',place.name);
+    if (!place.geometry || !place.geometry.location) return;
+
+    const marker = new google.maps.Marker({
+        gMap,
+        position: place.geometry.location,
+    });
+
+    google.maps.event.addListener(marker, "click", () => {
+        infowindow.setContent(place.name || "");
+        infowindow.open(gMap);
+    });
+}
+
 
 function onAddLocation(pos) {
     mapService.addLocation(pos);
@@ -90,9 +128,9 @@ function onAddMarker() {
 function onGetLocs() {
     locService.getLocs()
         .then(locs => {
-            console.log('Locations:', locs)
-            document.querySelector('.locs').innerText = JSON.stringify(locs)
-        })
+            console.log('Locations:', locs);
+            document.querySelector('.locs').innerText = JSON.stringify(locs);
+        });
 }
 
 function onGetUserPos() {
@@ -100,11 +138,11 @@ function onGetUserPos() {
         .then(pos => {
             console.log('User position is:', pos.coords);
             document.querySelector('.user-pos').innerText =
-                `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
+                `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`;
         })
         .catch(err => {
             console.log('err!!!', err);
-        })
+        });
 }
 function onPanTo() {
     console.log('Panning the Map');
